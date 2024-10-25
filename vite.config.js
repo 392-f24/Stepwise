fcc84ac5130d3acad30bb4fed20385c931dfd9a9
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-24 19:25:19 -0500

    doc: set up shortcut

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@context': path.resolve(__dirname, './src/context'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
