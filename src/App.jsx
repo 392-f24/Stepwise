58139b0042ae90dc4d7851f63c0602109da3184b
Elara Liu <40444637+ZL-Asica@users.noreply.github.com>
2024-10-26 15:36:42 -0500

    feat: Router and homepage (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,55 @@
 import { useState } from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
+import FlagIcon from '@mui/icons-material/Flag';
+import HomeIcon from '@mui/icons-material/Home';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '@utils/theme';
 
+import Home from './pages/Home';
+
 import './App.css';
-import logo from './logo.svg';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello Vite + React!</p>
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
-          </p>
-          <p>
-            Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </header>
+        <Router>
+          <div className="content">
+            {/* Main content area where pages will render */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/streak" element={<Streak />} />
+            </Routes>
+          </div>
+
+          {/* Bottom Navigation with React Router links */}
+          <BottomNavigation
+            className="bottom-nav"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            showLabels
+          >
+            <BottomNavigationAction label="Home" icon={<HomeIcon />} component={Link} to="/" />
+            <BottomNavigationAction
+              label="Steak"
+              icon={<FlagIcon />}
+              component={Link}
+              to="/streak"
+            />
+          </BottomNavigation>
+        </Router>
       </div>
     </ThemeProvider>
   );
 };
 
 export default App;
+
+const Streak = () => <h1>Streak</h1>;
