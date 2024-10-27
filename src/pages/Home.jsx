63af6dc408b6b4e21dc16b62d99dc4d02ad1f972
MC2b6 <micaserio606@gmail.com>
2024-10-27 14:23:32 -0500

    Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,10 @@
-import { handleSignIn, handleSignOut } from '@utils/auth';
-import { useNavigate } from 'react-router-dom';
+import Header from '@components/common/Header';
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const logIn = async () => {
-    const existingUser = await handleSignIn();
-    if (existingUser) {
-      console.log('Welcome back, existing user!');
-    } else {
-      console.log('New user profile created.');
-    }
-  };
-
-  const logOut = async () => {
-    await handleSignOut(navigate);
-  };
-
   return (
     <div>
+      <Header />
       <h1>Home</h1>
-      <button onClick={logIn}>Log In</button>
-      <button onClick={logOut}>Log Out</button>
     </div>
   );
 };
