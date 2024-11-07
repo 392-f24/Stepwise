d0e922be223c1cc40236ce3596e32e459900bdfe
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-06 20:05:10 -0600

    refactor: Move all GoalTracker into Home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import GoalTracker from '@/components/Home/GoalTracker'
+import AddItem from '@/components/Home/AddItem'
+import MacroGoal from '@/components/Home/MacroGoal'
+import { useUser } from '@/contexts/UserContext'
+import useGoalsUpdater from '@/hooks/useGoalsUpdater'
 import { Box } from '@mui/material'
 
 const Home = () => {
+  const { user } = useUser()
+  const { addGoal } = useGoalsUpdater()
+
   return (
-    <Box sx={{ width: '95%', margin: 'auto' }}>
-      <GoalTracker />
+    <Box sx={{ maxWidth: 800, margin: 'auto', padding: 2 }}>
+      <AddItem label='New Goal' onAdd={addGoal} />
+      <Box sx={{ height: 16 }} />
+      {user.goals.map((macroGoal, index) => (
+        <MacroGoal key={index} macroGoal={macroGoal} macroGoalIndex={index} />
+      ))}
     </Box>
   )
 }
